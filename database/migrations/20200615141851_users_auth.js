4f8848a8e7417d3813fd698711d690e75b945aa4
Sandra Coburn <sandra_coburn@hotmail.com>
2020-06-15 14:23:01 -0700

    Added migration table for users

---
+++ database/migrations/20200615141851_users_auth.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (users) => {
+    users.increments();
+
+    users.string("username", 128).notNullable().unique().index();
+    users.string("password", 128).notNullable();
+    users.string("projects", 256);
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
