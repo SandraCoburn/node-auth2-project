e76e261d1fd737846814e0de2c8ec33b7b07a5d2
Sandra Coburn <sandra_coburn@hotmail.com>
2020-06-15 14:03:43 -0700

    Added restricted-middleware

---
+++ auth/auth-router.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/users-model.js");
+
+// for endpoints beginning with /api/auth
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then((saved) => {
+      req.session.loggedIn = true; // for the session to show if logged in
+      res.status(201).json(saved);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        // req.session.userId = user.id;
+        req.session.username = user.username;
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(500).json({ you: "can check out anuy time you like" });
+      } else {
+        res.end(); // or res.stattus(200).json({you: "logged out successfully"})
+      }
+    });
+  } else {
+    res.status(200).json({ bye: "user" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,49 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
+const session = require("express-session"); //npm i express-session
+const KnexStore = require("connect-session-knex")(session); //remember to curry and pass the session
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require("../auth/restricted-middleware");
+const knex = require("../database/dbConfig"); // needed for storing session in the database
+
+const server = express();
+
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it safe!",
+  resave: true,
+  saveUninitialized: true, // reladed to GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10, //it's in miniseconds, 1000 to  one second times 60 time 10 to get 10 min
+    secure: false, // should be true in production
+    httpOnly: true, //true means JS can't touch the cookie
+  },
+  //remember the new keyword
+  store: new KnexStore({
+    knex,
+    tablename: "sessions",
+    createtable: true, //if there is no table in databease it will create it
+    sidfieldname: "sid",
+    clearInterval: 1000 * 60 * 15, //to clear the database after 15 min
+  }),
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig)); //to add sessions to log in, turn on the session midleware
+// at this poin there is a req.session object created by express-session
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
+
+server.get("/", (req, res) => {
+  console.log(req.session);
+  res.json({ api: "up" });
+});
+
+module.exports = server;
